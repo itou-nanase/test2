5d6877a9e48818c651a14e6b993a10acad8c9b28
itou <itou-n888@outlook.jp>
2026-03-15 12:50:20 +0900

    修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProductSeeder::class);
+    
+        $this->call([
+        SeasonSeeder::class,
+        ]);
     }
 }
